f471d8597e9469e442c97af232459a15540b7f02
Valentin <valentin.lucet@gmail.com>
2022-10-10 18:25:01 -0400

    improve constructor

--- src/types.jl
+++ src/types.jl
@@ -5,9 +5,16 @@ For now, a simple rootpath with a given image count.
 """
 struct Survey
     root::String
+    images::Array{String}
     image_count::Int
 
-    function Survey(root::String, image_count::Int)
-        new(root, image_count)
+    function Survey(root::String, pattern::String = ".jpg" ; absolute = true)
+        if isdir(root) 
+            images = read_dir(root, pattern ; absolute = absolute)
+        else
+            throw("Path $(root) is not a directory.")
+        end
+        image_count = length(images)
+        new(root, images, image_count)
     end
 end
