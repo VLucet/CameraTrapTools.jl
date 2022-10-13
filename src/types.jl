c7c344df6c496214db26584730eaad78a0dd7b46
Valentin <valentin.lucet@gmail.com>
2022-10-13 18:19:33 -0400

    update class

--- src/types.jl
+++ src/types.jl
@@ -1,21 +1,68 @@
 """
-    A camera trap survey
+    Camera trap Survey
 
-For now, a simple rootpath with a given image count.
+TBC
 """
 # TODO include survey structure with a set of dicts
 struct Survey
+    name::String
     root::String
     images::Array{String}
     image_count::Int
 
-    function Survey(root::String, pattern::String = ".jpg" ; absolute = true)
-        if isdir(root) 
-            images = read_dir(root, pattern ; absolute = absolute)
+    function Survey(name::String, root::String)
+        if isdir(root)
+            new(name, root, images, image_count)
         else
             throw("Path $(root) is not a directory.")
         end
-        image_count = length(images)
-        new(root, images, image_count)
     end
+end
+
+"""
+    Read Survey directory recursively
+
+This function reads all files in a Survey directory and returns an array of file 
+paths.
+"""
+function scan(rootpath::String, pattern::String = ".jpg"; absolute::Bool = true)
+
+    pattern = make_pattern(pattern)
+
+    file_list = String[]
+
+    for (root, dirs, files) in ScanDir.walkdir(rootpath)
+
+        if isempty(dirs) & !isempty(files)
+
+            relative_root = replace(root, rootpath => "")
+            @info "Listing files in $(relative_root)"
+
+            # TODO potential speedup: list comprehension, maybe using Glob.jl?
+            for file in files
+                if occursin(pattern, file)
+                    file_path =
+                        absolute ? joinpath(root, file) : joinpath(relative_root, file)
+                    push!(file_list, file_path)
+                end
+            end
+
+        end
+    end
+
+    return file_list
+
+end
+
+function scan(survey::Survey, pattern::String = ".jpg"; absolute::Bool = true)
+
+    rootpath = survey.root
+    name = survey.name
+
+    @info "Scanning survey '$(name)' at root folder $(rootpath)"
+
+    images = scan(rootpat, pattern; absolute = absolute)
+    image_count = length(images)
+
+    return((images, image_count))
 end
--- src/utils.jl
+++ src/utils.jl
@@ -1,38 +1,4 @@
 """
-    Read directory recursively
-
-This function reads all files in a directory and returns an array of file paths.
-"""
-function read_dir(rootpath::String, pattern::String = ".jpg" ; 
-                  absolute::Bool = true)
-
-    pattern = make_pattern(pattern)
-
-    file_list = String[]
-
-    for (root, dirs, files) in ScanDir.walkdir(rootpath)
-
-        if isempty(dirs) & !isempty(files)
-            
-            relative_root = replace(root, rootpath => "" )
-            @info "Listing files in $(relative_root)"
-            
-            # TODO potential speedup: list comprehension, maybe using Glob.jl?
-            for file in files
-                if occursin(pattern, file)
-                    file_path = absolute ? joinpath(root, file) : joinpath(relative_root, file)
-                    push!(file_list, file_path) 
-                end
-            end
-
-        end
-    end
-
-    return file_list
-
-end
-
-"""
     Produce a regex pattern
 
 For now, simple r"pattern" is generated.
